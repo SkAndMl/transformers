2f15b80435cce3a51cc70f61e8a52e3895c06f3d
Sathya Krishnan <ts.sathyakrishnan2002@gmail.com>
2023-07-19 00:49:08 +0530

    made train.py code device agnostic

--- gpt/train.py
+++ gpt/train.py
@@ -8,14 +8,18 @@ import json
 
 from model import PoemGPT
 
-with open("../config/poem_gpt_config.json", "r") as f:
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+with open("./poem_gpt_config.json", "r") as f:
     config = json.load(f)
 
-with open("./data/input.txt", "r", encoding="utf-8") as f:
+with open("./input.txt", "r", encoding="utf-8") as f:
     data = f.read()
 
 
 chars = sorted(list(set(data)))
+vocab_size = len(chars)
 stoi = {ch:i for i,ch in enumerate(chars)}
 itos = {i:ch for i,ch in enumerate(chars)}
 
@@ -40,6 +44,7 @@ def get_random_batch(split: str="train") -> Tuple[torch.Tensor, torch.Tensor]:
     x_batch = torch.stack([data[i:i+block_size] for i in idxs])
     y_batch = torch.stack([data[i+1:i+block_size+1] for i in idxs])
 
+    x_batch, y_batch = x_batch.to(device), y_batch.to(device)
     return x_batch, y_batch
 
 
@@ -64,9 +69,9 @@ def eval_model() -> Dict[str, float]:
 
 def train_poem_gpt():
 
-    for iter in random(config["train_iters"]):
+    for iter in range(config["train_iters"]):
 
-        if iter%config["eval_iterval"]==0:
+        if iter%config["eval_interval"]==0:
             losses = eval_model()
             print(f"iter {iter} train_loss: {losses['train']} val_loss: {losses['val']}")
         
@@ -78,7 +83,8 @@ def train_poem_gpt():
         optimizer.step()
 
 
-poem_gpt = PoemGPT(config)
+poem_gpt = PoemGPT(config, vocab_size)
+poem_gpt = poem_gpt.to(device)
 optimizer = torch.optim.AdamW(params=poem_gpt.parameters(),
                               lr=config["learning_rate"])
 
