0e779f102e30c70b442e7570e2db1d7873a95100
Sathya Krishnan <ts.sathyakrishnan2002@gmail.com>
2023-07-19 09:35:43 +0530

    gen poem script

---
+++ gpt/gen_poem.py
@@ -0,0 +1,40 @@
+import torch
+import json
+import sys
+import time
+import argparse
+from model import PoemGPT
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+with open("../config/poem_gpt_config.json") as f:
+    config = json.load(f)
+
+with open("./data/input.txt", "r", encoding="utf=8") as f:
+    data = f.read()
+    chars = sorted(list(set(data)))
+    vocab_size = len(chars)
+    itos = {i: ch for i, ch in enumerate(chars)}
+
+decode = lambda l: "".join([itos[i] for i in l]) 
+
+poem_gpt = PoemGPT(config=config,
+                   vocab_size=vocab_size)
+poem_gpt.load_state_dict(torch.load("./weights/poem_gpt_weights.pt",
+                                    map_location=torch.device(device=device)))
+
+
+
+def print_like_gpt(text):
+
+    for ch in text:
+        print(ch, end="", flush=True,)
+        time.sleep(0.02)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        num_tokens = int(sys.argv[2])
+    else:
+        num_tokens = 500
+    out = poem_gpt.generate(max_new_tokens=num_tokens) # [1, S]
+    text = decode(out[0].cpu().numpy())[1:]
+    print_like_gpt(text=text)
